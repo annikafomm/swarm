eac9f5d1b3bb5897576ae2c29fe4347c6b37797c
annikafomm <fommannika@gmail.com>
2025-06-26 09:00:04 +0200

    Include scores into geojson parse script

--- backend/visium_to_geojson.py
+++ backend/visium_to_geojson.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import scanpy as sc
-import squidpy as sq
 import geopandas as gpd
 
 
@@ -15,17 +14,29 @@ class Hexagons:
         self.scale = scale
         self.data_type = data_type
         self.coordinates, self.centers = self.parse_coordinates()
-        self.properties = self.parse_properties()
-        
-       
-    
-    def parse_properties(self):
+        self.obs = self.parse_obs()
+
+
+
+    def parse_obs(self):
         obs = self.anndata.obs
         properties = obs.copy()
         # Convert to dict with id as key and properties as values
         properties_dict = properties.to_dict(orient='index')
         return properties_dict
+  
+       
     
+    def parse_aucell_gene_sets(self):
+        uns = self.anndata.uns
+        gene_sets = uns.get("genesets", None)
+        if gene_sets is not None:
+            return gene_sets
+        else:
+            print("No Aucell gene sets found in the AnnData object.")
+            return {}
+            
+
     def hexagon_points(self,x, y, radius):
         # Scale centers
         x = x * self.scale
@@ -34,7 +45,7 @@ class Hexagons:
             (x + radius * np.cos(np.pi / 3 * i), y + radius * np.sin(np.pi / 3 * i))
             for i in range(6)
         ] + [(x + radius * np.cos(0), y + radius * np.sin(0))]
-        
+
     def parse_coordinates(self):
         if self.data_type == "visium":
             anndata_spatial_coordinates = self.anndata.obsm["spatial"].copy()
@@ -57,7 +68,7 @@ class Hexagons:
             raise ValueError(f"Unsupported data type: {self.data_type}. Supported types are 'visium' and 'xenium'.")
 
 
-    
+
     def to_geojson(self):
         hexagons = {
                 "type": "FeatureCollection",
@@ -65,39 +76,50 @@ class Hexagons:
             }
         for barcode, coords in zip(self.anndata.obs.index, self.coordinates):
 
-            if self.properties[barcode]["in_tissue"] == False:
+            if self.obs[barcode]["in_tissue"] == False:
                 continue
 
-            hexagons["features"].append(
-                {"type": "Feature",
+            leiden_cluster = int(self.anndata.obs.get("leiden", {}).get(barcode, None))
+
+            leiden_centrality = self.anndata.uns.get("leiden_centrality_scores", {}).iloc[leiden_cluster].to_dict()
+            leiden_co_occurrence = self.anndata.uns.get("leiden_co_occurrence")["occ"][leiden_cluster]
+            leiden_nhood_zscores = self.anndata.uns.get("leiden_nhood_enrichment",{}).get("zscore")[leiden_cluster]
+
+            # Print all types
+            print(type(leiden_centrality))
+            print(type(leiden_co_occurrence))
+            print(type(leiden_nhood_zscores))
+
+            feature_dict = {"type": "Feature",
                 "geometry": {
                     "type": self.geometry_type,
                     "coordinates": [coords[barcode]]
                 },
                 "properties": {
                     "barcode": barcode,
-                    "centroid": self.centers[barcode],
-                    "cell_type": self.properties[barcode].get("cell_type", "unknown")
+                    "aucell": self.anndata.obsm.get("aucell", {}).get(barcode, {}),
+                    "aucell_genesets": self.anndata.obsm.get("aucell_genesets", {}),
+                    "leiden": leiden_cluster,
+                    "leiden_centrality": leiden_centrality,
+                    "leiden_co_occurrence": leiden_co_occurrence.tolist(),
+                    "leiden_nhood_enrichment": leiden_nhood_zscores.tolist()
                     }
                 }
-            )
-        
+            
+            # Add additional properties from obs
+            for key, value in self.obs[barcode].items():
+                feature_dict["properties"][key] = value
+
+            # Add leiden properties from uns
+            
+
+            
+            hexagons["features"].append(feature_dict)
+
         return hexagons
     
 
-class Counts:
-    def __init__(self, anndata):
-        self.anndata = anndata
-        self.counts = self.parse_counts()
 
-    def parse_counts(self):
-        counts = self.anndata.X.toarray() if hasattr(self.anndata.X, "toarray") else self.anndata.X
-        genes = self.anndata.var.index.tolist()
-        barcodes = self.anndata.obs.index.tolist()
-        counts_df = pd.DataFrame(counts, index=barcodes, columns=genes)
-        return counts_df
-        
-    
 if __name__ == "__main__":
     import json
     import argparse
@@ -112,14 +134,12 @@ if __name__ == "__main__":
     spatial_data = sc.read_h5ad(args.adata)
 
     hexagons = Hexagons(spatial_data, radius=args.radius, scale=0.4, data_type=args.data_type)
-    counts = Counts(spatial_data)
 
     geojson_data = hexagons.to_geojson()
 
     with open("hexagons_GSM6592049_M2.geojson", "w") as f:
         json.dump(geojson_data, f, indent=4)
-        
-    print("GeoJSON file created successfully.")
-        
 
-        
+    print("GeoJSON file created successfully.")
+
+
