d91d998111a380b9ea93ea05b96dc29219d51c17
NoahNussbaumer <noah.nussbaumer03@gmail.com>
2025-08-12 10:13:25 +0200

    Move liana stuff to liana dir, prepare files for CLI
    Delete old files

--- backend/liana/liana_bivariate.py
+++ backend/liana/liana_bivariate.py
@@ -2,16 +2,21 @@ from itertools import product
 
 import decoupler as dc
 import liana as li
-import numpy as np
 import pandas as pd
 import scanpy as sc
-from matplotlib import pyplot as plt
 from mudata import MuData
 
 
 def ligand_receptor_relationships(
     adata: sc.AnnData, return_scores: bool = False
 ):
+    # HACK: Setting index.name to None is required to fix
+    # common bug (MergeError). If everything goes down the
+    # drain because of that we might need to come back.
+    # https://github.com/saezlab/liana-py/issues/143
+    adata.var.index.name = None
+    adata.var.index = adata.var.index.str.upper()
+
     # Bivariate Ligand-Receptor Relationships
     # Parameters from tutorial
     lrdata = li.mt.bivariate(
@@ -85,6 +90,16 @@ def cell_comp_tf_activity_similarity(
     cell_comp_obsm_key: str = "tangram_ct_pred",
     return_scores: bool = False,
 ):
+    # Make sure the GRN column names are ok
+    net = net.rename(
+        columns={
+            old: new
+            for old, new in zip(
+                net.columns[:3], ["source", "target", "weight"]
+            )
+        }
+    )
+
     # Extract Cell type Composition
     comps = li.ut.obsm_to_adata(adata, cell_comp_obsm_key)
 
@@ -139,61 +154,3 @@ def cell_comp_tf_activity_similarity(
         return scores
     else:
         adata.obs = adata.obs.join(scores)
-
-
-if __name__ == "__main__":
-    # Source: https://liana-py.readthedocs.io/en/latest/notebooks/bivariate.html
-    # NOTE: Make sure that liana >= 1.0.0 is installed
-
-    # Data loading
-    # dataset_path = "/nfs/data3/mopitas/mapra/datasets/GSM6592049_M2/GSM6592049_M2.h5ad"
-    dataset_path = "/home/noah/Downloads/tangram_adata.h5ad"
-    c2l_path = "/home/noah/Downloads/cell2location_adata.h5ad"
-    adata = sc.read(dataset_path)
-    c2l_adata = sc.read(c2l_path)
-    adata.obsm["c2l_compositions"] = c2l_adata.obsm["q05_cell_abundance_w_sf"]
-
-    # Only look at spots in tissue
-    adata = adata[adata.obs["in_tissue"] == 1]
-    # Make sure that all cells have counts, else decoupler's ULM throws them
-    # out with just a warning, causing annoying shape mismatches
-    sc.pp.filter_cells(adata, min_counts=1)
-
-    # Normalization from tutorial
-    adata.layers["counts"] = adata.X.copy()
-    sc.pp.normalize_total(adata, target_sum=1e4)
-    sc.pp.log1p(adata)
-
-    # Spatial connectivity graph
-    plot, bw_df = li.utils.query_bandwidth(
-        coordinates=adata.obsm["spatial"], start=0, end=500, interval_n=20
-    )
-    bw = int(np.round(bw_df[bw_df["neighbours"] >= 6].iloc[0, 0]))
-
-    # Bandwidth may be set based on query_bandwidth function
-    li.ut.spatial_neighbors(
-        adata, bandwidth=bw, cutoff=0.1, kernel="gaussian", set_diag=True
-    )
-
-    # NOTE: Setting index.name to None is required to fix
-    # common bug (MergeError)
-    # https://github.com/saezlab/liana-py/issues/143
-    adata.var.index.name = None
-    adata.var.index = adata.var.index.str.upper()
-
-    ligand_receptor_relationships(adata)
-
-    # net = dc.op.collectri()
-    net = pd.read_csv(
-        "/home/noah/Downloads/genie3_BRCA_mrn.top_100k.csv",
-    )
-    net = net.rename(
-        columns={
-            old: new
-            for old, new in zip(net.columns, ["source", "target", "weight"])
-        }
-    )
-
-    cell_comp_tf_activity_similarity(adata, net, "c2l_compositions")
-
-    adata.write("/home/noah/Downloads/liana_cell2location_adata.h5ad")
--- backend/liana/run_liana.py
+++ backend/liana/run_liana.py
@@ -1,3 +1,8 @@
+"""
+Source: https://liana-py.readthedocs.io/en/latest/notebooks/bivariate.html
+NOTE: Make sure that liana >= 1.0.0 is installed
+"""
+
 import argparse
 
 import decoupler as dc
@@ -14,22 +19,20 @@ if __name__ == "__main__":
     parser.add_argument("--adata", type=str, required=True)
     parser.add_argument("--grn", type=str, default=None)
     parser.add_argument("--pathway-net", type=str, default=None)
+    parser.add_argument("--output", type=str, required=True)
 
     args = parser.parse_args()
 
     adata = sc.read_h5ad(args.adata)
 
-    local_lr_scores, global_lr_scores, factor_scores = (
-        ligand_receptor_relationships(adata, return_scores=True)
-    )
+    ligand_receptor_relationships(adata)
 
     grn = (
         pd.read_csv(args.grn) if args.grn is not None else dc.op.collectri(),
     )
-    comp_tf_scores = cell_comp_tf_activity_similarity(
+    cell_comp_tf_activity_similarity(
         adata,
         grn,
-        return_scores=True,
     )
 
     pathway_net = (
@@ -37,4 +40,6 @@ if __name__ == "__main__":
         if args.pathway_net is not None
         else dc.op.progeny()
     )
-    pathway_scores = pathway_activities(adata, pathway_net, return_scores=True)
+    pathway_activities(adata, pathway_net)
+
+    adata.write_h5ad(args.output)
