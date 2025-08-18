c9356a2e20a6affbabc482a184ef670344322ce6
Dibassow <104676222+Dibassow@users.noreply.github.com>
2025-08-18 12:43:57 +0000

    add information page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormPageComponent } from './form-page/form-page.component';
-import { HexagonPlotComponent } from './hexagon-plot/hexagon-plot.component';
+//import { HexagonPlotComponent } from './hexagon-plot/hexagon-plot.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HexagonPlotComponent, CommonModule, FormPageComponent],
+  standalone: true,
+  imports: [CommonModule, FormPageComponent, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
