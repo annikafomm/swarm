ee794e2d08d1826ebb9600868810576f2ce06c33
merit <merit.back@tum.de>
2026-03-09 17:01:19 +0100

    port issues

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true, // Indicates that this is the production build.
-
-  // Base URL for API requests in production.
-  // Should point to the deployed backend service.
-  apiBaseUrl: 'http://0.0.0.0:3005',
-};
