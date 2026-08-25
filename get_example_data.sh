b3f15cc20c03bd91f8f0a6504a0c32101b9edc35
Lena Strasser <strasser.lena@gmx.de>
2026-08-25 10:00:43 +0000

    docs: add script to download example dataset from Google Drive

---
+++ get_example_data.sh
@@ -0,0 +1,12 @@
+# download example data from google drive
+
+mkdir data
+cd data
+
+wget -O adata.h5ad "https://drive.usercontent.google.com/download?id=1WBN1z3zlOBnu58jOvcBeWcerFviHffh9&export=download&confirm=yes"
+
+wget -O genie_network_filt.csv "https://drive.usercontent.google.com/download?id=1LqtKT65_jMp0uevbNu3OsKC0bsnQE6yB&export=download&confirm=yes"
+
+wget -O hexagons.geojson "https://drive.usercontent.google.com/download?id=1zQyAk-lfY8SdmF4gfvXFcmWBL85dJWWY&export=download&confirm=yes"
+
+wget -O sponge_network_smaller.csv "https://drive.usercontent.google.com/download?id=1qHhdBJTTH5kFdAeX6FvwfM4Ji9twes-O&export=download&confirm=yes"
